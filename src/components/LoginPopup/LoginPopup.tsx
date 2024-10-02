dbccc6ab68a8e7f93eb8e16e017698ed50b6f180
letovDmitry <dbaltsezhak1@bk.ru>
2024-10-02 19:42:44 +0300

    kdsaf

--- src/components/LoginPopup/LoginPopup.tsx
+++ src/components/LoginPopup/LoginPopup.tsx
@@ -153,7 +153,11 @@ const LoginPopup = () => {
               <LoginButton
                 botUsername={"sadjxjcvjxzucvu_bot"}
                 onAuthCallback={(data) => {
-                  signIn("telegram-login", { callbackUrl: "/" }, data as any);
+                  signIn(
+                    "telegram-login",
+                    { callbackUrl: "/profile" },
+                    data as any
+                  );
                 }}
                 buttonSize="small" // "large" | "medium" | "small"
                 cornerRadius={5} // 0 - 20
